e00f2d08aa7f9e1412e729c71cf7c04ec61d4055
Aaqib <150870@students.au.edu.pk>
2018-04-28 18:28:07 +0500

    First Commit
    Lab 7 worked done

---
+++ lab 7/Assets/Scripts/Scale.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scale : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.localScale = new Vector3(5, 2, 9);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
